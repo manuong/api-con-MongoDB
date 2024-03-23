fe615a61f1ade6adc2756e7d599dc79981b9ea99
Emmanuel <manumtz20@gmail.com>
2024-03-23 16:01:47 -0600

    connectionString por variables de entorno

--- src/mongodb.js
+++ src/mongodb.js
@@ -1,11 +1,7 @@
 const mongoose = require('mongoose');
-const { PASSWORD_DB, USER_DB, CLUSTER_DB, NAME_DB } = process.env;
 
 // connectionString es proporcionada por Atlas (BD en la nube) o por mongodb localmenten
-const connectionString = `mongodb+srv://${USER_DB}:${PASSWORD_DB}@${CLUSTER_DB}.ismuwtp.mongodb.net/${NAME_DB}?retryWrites=true&w=majority&appName=notesapi`;
-
-// const connectionString =
-//   'mongodb+srv://manuong:guitardata2024@manuclusters.ismuwtp.mongodb.net/<nombre de la base de datos>?retryWrites=true&w=majority&appName=notesapi';
+const connectionString = process.env.MONGODB_URI;
 
 // conexion a mongodb
 const connectDB = mongoose
