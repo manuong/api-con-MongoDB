d50dc866a7ab6c61e04184a79ff8e7741453742d
Emmanuel <manumtz20@gmail.com>
2024-03-23 13:38:30 -0600

    correccion de conecctionString

--- src/mongodb.js
+++ src/mongodb.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 const { PASSWORD_DB, USER_DB, CLUSTER_DB, NAME_DB } = process.env;
 
-const connectionString = `mongodb+srv://${USER_DB}:${PASSWORD_DB}@${CLUSTER_DB}.ismuwtp.mongodb.net/?retryWrites=true&w=majority&appName=${NAME_DB}`;
+// connectionString es proporcionada por Atlas (BD en la nube) o por mongodb localmenten
+const connectionString = `mongodb+srv://${USER_DB}:${PASSWORD_DB}@${CLUSTER_DB}.ismuwtp.mongodb.net/${NAME_DB}?retryWrites=true&w=majority&appName=notesapi`;
 
 // const connectionString =
-//   'mongodb+srv://manuong:guitardata2024@manuclusters.ismuwtp.mongodb.net/?retryWrites=true&w=majority&appName=notesapi';
+//   'mongodb+srv://manuong:guitardata2024@manuclusters.ismuwtp.mongodb.net/<nombre de la base de datos>?retryWrites=true&w=majority&appName=notesapi';
 
 // conexion a mongodb
 const connectDB = mongoose
