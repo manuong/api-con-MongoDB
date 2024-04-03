95e4efa9202c79ac7d5c12f896fe7a2bdf3917f9
Emmanuel <manumtz20@gmail.com>
2024-04-03 17:51:25 -0600

    ruta para traer los detalles de un solo usuario

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,5 +1,19 @@
-const getUsersController = async (req, res) => {
-  res.send('get users');
+const User = require('../models/user.model');
+
+const getUsersController = async (req, res, next) => {
+  const { userId } = req.params;
+
+  try {
+    if (userId) {
+      const userFound = await User.findById(userId);
+      return res.status(200).json(userFound);
+    }
+
+    const allUsers = await User.find();
+    res.status(200).json(allUsers);
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,5 +2,6 @@ const userRoutes = require('express').Router();
 const { getUsersController } = require('../controllers/user.controllers');
 
 userRoutes.get('/users', getUsersController);
+userRoutes.get('/users/:userId', getUsersController);
 
 module.exports = userRoutes;
