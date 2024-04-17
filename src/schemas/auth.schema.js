a76a1cd1da8a271433002279023dd035a5605cba
Emmanuel <manumtz20@gmail.com>
2024-04-16 23:25:23 -0600

    esquema de validacion para autenticar usuario

---
+++ src/schemas/auth.schema.js
@@ -0,0 +1,62 @@
+// utilizando el framework "zod" para validaciones de datos
+const { z } = require('zod');
+
+// "z" es un objeto que nos va a permitir dar tipos de datos
+
+// para validar el objeto que nos llega por body al registrarse un nuevo usuario, definimos los datos
+const registerSchema = z.object({
+  username: z
+    .string({
+      required_error: 'El nombre de usuario es requerido', // mensaje cuando es una dato requerido
+    }) // se puede definir longitud minima de caracteres
+    .min(3, {
+      message: 'El nombre de usuario es muy corto',
+    }),
+  email: z
+    .string({
+      required_error: 'El email es requerido',
+    })
+    .email({
+      message: 'email invalido', // mensaje de error si falla la validacion
+    }),
+  password: z
+    .string({
+      required_error: 'La contraseña es requerida',
+    })
+    .min(6, {
+      message: 'La contraseña debe contener minimo 6 caracteres',
+    }),
+});
+
+const loginSchema = z
+  .object({
+    username: z.string().optional(),
+    email: z
+      .string()
+      .email({
+        message: 'email invalido',
+      })
+      .optional(),
+    password: z
+      .string({
+        required_error: 'La contraseña es requerida',
+      })
+      .min(6, {
+        message: 'La contraseña debe contener minimo 6 caracteres',
+      }),
+  }) // metodo .refine() para hacer validaciones complejas, personalizadas o condiciones adicionales a las que ya tiene "zod"
+  .refine(
+    (data) => {
+      if (data.username || data.email) {
+        return true;
+      } else return false;
+    },
+    {
+      message: 'Se requiere nombre de usuario o email',
+    }
+  );
+
+module.exports = {
+  registerSchema,
+  loginSchema,
+};
---
+++ teoria/validaciones.md
@@ -0,0 +1,20 @@
+# Validacion de Datos
+
+Las validaciones se pueden hacer manualmente, pero tambien existen frameworks que podemos utilizar para hacer validaciones, en este proyecto se va a utilizar Zod
+
+### Zod
+
+es una biblioteca de validación de esquemas para JavaScript y TypeScript. Se utiliza para definir y validar la estructura de los datos en aplicaciones, asegurando que cumplan ciertos criterios antes de ser utilizados o almacenados. Algunos de los casos de uso comunes incluyen:
+
+- Validación de datos de entrada del usuario en formularios web.
+- Validación de datos recibidos de una API antes de procesarlos.
+- Verificación de la integridad de los datos antes de almacenarlos en una base de datos.
+- Definición y verificación de la estructura de objetos en aplicaciones complejas.
+
+Zod proporciona una forma sencilla y declarativa de definir esquemas de datos y realizar validaciones, lo que hace que sea más fácil garantizar la consistencia y la integridad de los datos en una aplicación. Además, al ser compatible con TypeScript, Zod ofrece un fuerte soporte de tipos, lo que significa que los errores de validación se pueden detectar en tiempo de compilación, lo que ayuda a evitar errores en tiempo de ejecución.
+
+su intalacion
+
+```bash
+npm install zod
+```
